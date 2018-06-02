476aca56b9f00ed7b6c5941a4cb89cbb9de60531
gaoxiang <gaoxiang@ezbuy.com>
2018-06-02 23:55:10 +0800

    使用模板字符串拼接字符串

--- nodeJS/User/index.js
+++ nodeJS/User/index.js
@@ -34,8 +34,11 @@ app.get("/verifyCode", function (req, res) {
         return
     }
 
-    // 2. 检查改用户是否已经注册过
-    let sql = 'SELECT COUNT(ID) AS id FROM USER WHERE PHONENUMBER =' + phoneNumber;
+    // 2. 检查改用户是否已经注册过${expression}
+    
+    let sql = `SELECT COUNT(ID) AS id FROM USER WHERE PHONENUMBER = ${ phoneNumber }`;
+    console.log(sql);
+    
     db.query(sql, function (error, data, fileds) {
         if (error) {
             res.status(500).send("服务器端内部错误").end()
@@ -52,7 +55,7 @@ app.get("/verifyCode", function (req, res) {
         // 3. 发送验证码
         //  用一张表记录验证码,然后验证, 直接插入即可
         let code = "123456"
-        var sql = "INSERT INTO TmpVerfiyCode(ID, PHONENUMBER, CODE) VALUES(0," + phoneNumber + ", " + code + ");";
+        var sql = `INSERT INTO TmpVerfiyCode(ID, PHONENUMBER, CODE) VALUES(0,+ ${phoneNumber}, ${code});`;
         console.log(sql);
         
 
@@ -78,9 +81,7 @@ app.post("/register", function (req, res, next) {
     const password = body.password
 
     // 验证是否有对应的
-    let sql = 'SELECT EXISTS(SELECT * FROM TmpVerfiyCode WHERE PHONENUMBER=' + phoneNumber + ' AND CODE=' + code + ') AS exist';
-    console.log(sql);
-    
+    let sql = `SELECT EXISTS(SELECT * FROM TmpVerfiyCode WHERE PHONENUMBER= ${phoneNumber}  AND CODE= ${ code })  AS exist`;    
     db.query(sql, function (error, data, fileds) {
         if (error) {
             console.log(error);
@@ -103,7 +104,8 @@ app.post("/register", function (req, res, next) {
             return 
         }
 
-        // 写入到用户的数据库      
+        // 写入到用户的数据库     
+        ` ` 
         let sql = util.format("INSERT INTO USER (ID, PHONENUMBER, PASSWORD) VALUES(0, %s, %s)", phoneNumber, password)
         db.query(sql, function (error, data, fileds) {
             if (error) {
@@ -121,4 +123,3 @@ app.post("/register", function (req, res, next) {
 app.listen(9090)
 
 console.log("Start Server, Port is ", 9090)
-
